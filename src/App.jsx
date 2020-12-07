8c40bbabb8c6c88600a5738fc73798086d621279
ohmyskyhigh <miaorunkun@163.com>
2020-12-07 18:00:30 +0800

    context

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import './App.css';
 import PropTypes from 'prop-types'
 
 const BatteryContext = createContext();
+const SwitchContext = createContext();
 
 class Leef extends Component {
+  constructor(props){
+    super(props);
+    static contextType = BatteryContext;
+  }
+
   render () {
+    const {battery} = this.context;
     return (
-      <BatteryContext.Consumer >
-        {
-          (battery) => <h1>Battery: {battery}</h1>
-        }
-      </BatteryContext.Consumer >
+      <h1>Battery: {battery}</h1>
     );
   };
 }
@@ -28,10 +31,30 @@ class Branch extends Component {
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      battery: 60,
+      trueFalse: true
+    }
+  }
+
   render(){
+    const {battery} = this.state
+    console.log(battery)
     return (
-      <BatteryContext.Provider value={60}>
+      <BatteryContext.Provider value={battery}>
         <Branch />
+        <button
+          type='button'
+          style={{width:'60px'}}
+          onClick = {() =>{
+              this.setState(
+                {battery: battery-1}
+              )
+          }}>
+        press
+      </button>
       </BatteryContext.Provider>
     );
   }
---
+++ test.js
@@ -0,0 +1,5 @@
+function Foo(name) {
+	this.name = name
+	return 520
+}
+console.log(Foo('wscats'))
