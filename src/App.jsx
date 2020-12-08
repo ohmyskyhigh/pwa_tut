52e1e4ff3d604cc0097cebc0c00156328d1130c0
ohmyskyhigh <miaorunkun@163.com>
2020-12-08 17:16:07 +0800

    useEffect, useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 
-import react, {Component} from 'react';
+import react, {useEffect, useState} from 'react';
 import './App.css';
 import PropTypes from 'prop-types'
+
+
+function App (){
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('mike');
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={()=>
+          {setCount(count+1);
+           setName('john');}}
+      >
+        click here {count}
+      </button>
+      <div>
+        {name}
+      </div>
+    </div>
+
+  )
+}
+
 
 export default App;
